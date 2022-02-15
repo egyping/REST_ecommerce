99f5b76f3bb7ab009b814728ec1c3b4a13dd4a51
Fahd Mannaa <egypingblog@gmail.com>
2022-02-15 20:42:14 +0300

    POST PUT DELETE

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from rest_framework import serializers
 
 from store.models import Collection, Product # 2.5
@@ -5,25 +6,53 @@ from store.models import Collection, Product # 2.5
 from decimal import Decimal #2.7
 
 
-# 2.9 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    
-    
-# 2.5
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField() # 2.5
-    title = serializers.CharField(max_length=255) # 2.5
-    # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2) # 2.5
-    
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source= 'unit_price') # 2.8
-    
-    # 2.7
-    # SerializerMethodField means you can write your own method 
+# 2.11 > same like 2.10 just add the missing fields
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'collection', 'price_with_tax',
+                  'slug', 'inventory', 'description',]
+    #custom field
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.5)
     
-    # 2.9
-    collection = CollectionSerializer()
+# 2.10
+# class ProductSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Product
+#         fields = ['id', 'title', 'unit_price', 'collection', 'price_with_tax',]
+#     #custom field
+#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+#     def calculate_tax(self, product: Product):
+#         return product.unit_price * Decimal(1.5)
+
+# 2.10
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+    
+        
+# 2.9 
+# class CollectionSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+# 2.5
+# class ProductSerializer(serializers.Serializer):
+#     id = serializers.IntegerField() # 2.5
+#     title = serializers.CharField(max_length=255) # 2.5
+#     # unit_price = serializers.DecimalField(max_digits=6, decimal_places=2) # 2.5
+    
+#     price = serializers.DecimalField(max_digits=6, decimal_places=2, source= 'unit_price') # 2.8
+    
+#     # 2.7
+#     # SerializerMethodField means you can write your own method 
+#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+#     def calculate_tax(self, product: Product):
+#         return product.unit_price * Decimal(1.5)
+    
+#     # 2.9
+#     collection = CollectionSerializer()
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+
 from django.http import HttpResponse # 2.2
 from rest_framework.decorators import api_view # 2.3
 from rest_framework.response import Response # 2.3
@@ -6,21 +7,58 @@ from .serializers import ProductSerializer # 2.5
 
 from django.shortcuts import get_object_or_404 # 2.6
 
+from rest_framework import status # 2.11
+from logging import raiseExceptions # 2.11
 
-# 2.6
-@api_view()
+# # 2.11 
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        # take the data from the request and pass it to the serializer then validated 
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raiseExceptions==True)
+        # make sure to add the missing fields at the product serializer "mandatory fields"
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+#2.11 adding put methond to the detail 
+@api_view(['GET', 'PUT', "DELETE"])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    # serializer.data will convert product object to dictionary JSON 
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# 2.6
+# @api_view()
+# def product_detail(request, id):
+#     product = get_object_or_404(Product, pk=id)
+#     serializer = ProductSerializer(product)
+#     # serializer.data will convert product object to dictionary JSON 
+#     return Response(serializer.data)
+
+
+
 
 # # 2.6 
-@api_view()
-def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+# @api_view()
+# def product_list(request):
+#     queryset = Product.objects.all()
+#     serializer = ProductSerializer(queryset, many=True)
+#     return Response(serializer.data)
 
 
 # 2.5
